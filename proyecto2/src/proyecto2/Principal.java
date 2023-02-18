ad82bc9625371d19413f19a12247c971a5507b8a
Estudiante USC <estudiante@GTACAD.USC>
2023-02-18 16:04:09 -0500

    primer commit

---
+++ proyecto2/src/proyecto2/Principal.java
@@ -0,0 +1,9 @@
+package proyecto2;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+	Student std = new Student () ;
+	System.out.print(std.id + "\n" + std.name + "\n" + std.tatus);
+	}
+}
---
+++ proyecto2/src/proyecto2/Student.java
@@ -0,0 +1,9 @@
+package proyecto2;
+
+public class Student {
+	
+	String name = "Lisa Palombo";
+	int id = 123456789;
+	String tatus = "Active";
+
+}
